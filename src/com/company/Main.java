9ca078bda21314f32fb67ef93fd9f100b35ede4b
Aijanss <68231963+Aijanss@users.noreply.github.com>
2020-08-20 23:41:31 +0600

    Д З

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
         Boss2 boss2 = new Boss2(200, 100, "");
         boss2.setHealth(200);
         boss2.setDamage(100);
-        boss2.Defence = "Magic";
+        boss2.setDefence("Magic") ;
         System.out.println("Boss health " + boss2.getHealth() + "  Boss damage" + " " + boss2.getDamage() + "  "
                 + " Boss defence " + boss2.getDefence());
 
